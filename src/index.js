c0739f9fc91544b3b29ebe5ac7ebd826ccc3f545
Logeast <logeast@outlook.com>
2022-07-07 14:24:15 +0800

    update file import

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 import App from "./App";
-import Appv2 from "./Appv2";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <Appv2 />
-    {/* <App /> */}
+    <App />
   </StrictMode>,
   rootElement
 );
